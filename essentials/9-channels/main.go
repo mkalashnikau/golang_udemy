38f96f0c7dd6682ca45b92a86d009576c8157976
mkalashnikau <kalashnikovgomel@gmail.com>
2022-02-18 00:37:46 +0300

    Channel lesson is completed

--- essentials/9-channels/main.go
+++ essentials/9-channels/main.go
@@ -2,20 +2,25 @@ package main
 
 import (
 	"log"
+
 	"github.com/mkalashnikau/golang_udemy/helpers"
 )
 
-const numPool = 10
+const numPool = 1000
+
+// Channels are useful when our program has multiple packages
 
 func CalculateValue(intChan chan int) {
-	randomNumber := 
-	intChan <- helpers.RarandomNumber(numPool)
+	randomNumber := helpers.RandomNumber(numPool)
+	intChan <- randomNumber
 }
 
 func main() {
 	intChan := make(chan int)
+	// We say: close the channel once it executed
 	defer close(intChan)
 
+	// Goroutine (A goroutine is a lightweight thread managed by the Go runtime.)
 	go CalculateValue(intChan)
 
 	num := <-intChan
